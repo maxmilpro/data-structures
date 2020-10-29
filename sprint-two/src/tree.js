0ea62dd1150513f2a6cc035ffbd1f91776cf0588
Michael Pierce <pierce.michael.b@gmail.com>
2020-10-28 22:47:49 -0500

    Partially completed tree methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -18,6 +18,23 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  // if the value of the current node is equal to target
+  if (this.value === target) {
+    // return true
+    return true;
+  }
+  // debugger;
+  // iterate over the children of the current node
+  for (var i = 0; i < this.children.length; i++) {
+    // call contains on each child
+    return this.children[i].contains(target); // this.children[0].contains(target); === {value: 5, chidlren: [...]}.contains(target);
+  }
+
+  // _.each(this.children, function(value) {
+  //   return value.contains(target);
+  // });
+  // return false
+  return false;
 };
 
 
