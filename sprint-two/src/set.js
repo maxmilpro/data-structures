5dcbd550835d40158c432051be0428b5e7183e63
Michael Pierce <pierce.michael.b@gmail.com>
2020-10-31 12:05:05 -0500

    Refactored set methods

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,34 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = []; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  // if the item does not exist in the storage array and the item is a string
-  if (this._storage.indexOf(item) < 0 && typeof item === 'string') {
-    // add the item to the storage array
-    this._storage.push(item);
+  // if the item does not exist in the storage object and the item is a string
+  if (!this._storage[item] && typeof item === 'string') {
+    // add the item to the storage object
+    this._storage[item] = item;
   }
 };
 
 setPrototype.contains = function(item) {
-  // return if the index of the item in the storage array is greater than or equal to 0
-  return this._storage.indexOf(item) >= 0;
+  return this._storage[item] === item;
 };
 
 setPrototype.remove = function(item) {
-  // store the index of the input item
-  const itemIndex = this._storage.indexOf(item);
-  // remove the item from the storage array based on the index
-  this._storage.splice(itemIndex, 1);
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
-  add: linear time
-  contains: linear time
-  remove: linear time
+  add: constant time
+  contains: constant time
+  remove: constant time
  */
